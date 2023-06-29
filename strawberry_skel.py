4f4be796a343c525fa900426d3b1fcbb0a9efa8b
Katherine James <kajames@lincoln.ac.uk>
2023-06-29 16:11:10 +0100

    Save results

--- strawberry_skel.py
+++ strawberry_skel.py
@@ -12,6 +12,7 @@ import visualize as vis
 import somSkeleton as som
 import skeleton as skel
 import utils
+import os
 
 from data_loader import filter_xyz
 
@@ -35,15 +36,15 @@ def experiment_1_semantic():
     line_set = o3d.geometry.LineSet(points=o3d.utility.Vector3dVector(
             nodes), lines=o3d.utility.Vector2iVector(edges))
 
-    o3d.visualization.draw_geometries([pcd, line_set])
-    # o3d.io.write_line_set(
-    #         f"results_seedling4/skel_files/p2_cyl.ply", line_set, write_ascii=True)
+    # o3d.visualization.draw_geometries([pcd, line_set])
+    o3d.io.write_line_set(
+            f"Results/som/demo_semantic.ply", line_set, write_ascii=True)
 
 
 def experiment_2_instance():
     selected_cats = ["petiole"] 
     pcds = filter_xyz("demo", selected_cats, filter_instances=True)
-    for pcd in pcds:     
+    for i, pcd in enumerate(pcds):     
         data = np.array(pcd.points)
         cwise_skeleton_nodes = som.getSkeleton([data])
         graph = som.getGraph(cwise_skeleton_nodes, data)
@@ -56,9 +57,14 @@ def experiment_2_instance():
         line_set = o3d.geometry.LineSet(points=o3d.utility.Vector3dVector(
                 nodes), lines=o3d.utility.Vector2iVector(edges))
 
-        o3d.visualization.draw_geometries([pcd, line_set])
+        # o3d.visualization.draw_geometries([pcd, line_set])
+        o3d.io.write_line_set(
+            f"Results/som/demo_i{i}.ply", line_set, write_ascii=True)
 
 if __name__ == "__main__":
+
+    if not os.path.exists("Results/som"):
+        os.mkdir("Results/som")
 
     # Case 1: Semantic segmentation
     experiment_1_semantic()  
